38018ef56b5da397dc91c35bf9253e8fda97d81f
lyeiva86 <ulul10039@gmail.com>
2024-05-27 22:38:56 +0700

    feat: backend tambah jadwal olahraga

--- lib/viewmodels/jadwal_olahraga_viewmodel.dart
+++ lib/viewmodels/jadwal_olahraga_viewmodel.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:sikode/models/jadwal_olahraga.dart';
 
@@ -17,6 +20,33 @@ class JadwalOlahragaViewModel extends ChangeNotifier {
       notifyListeners();
     } catch (e) {
       print('Error fetching informasi: $e');
+    }
+  }
+
+  Future<String> uploadImageToStorage(File imageFile, String imageName) async {
+    try {
+      final Reference storageReference = FirebaseStorage.instance.ref('jadwal_olahraga_thumbnail').child(imageName);
+      final UploadTask uploadTask = storageReference.putFile(imageFile);
+      final TaskSnapshot taskSnapshot = await uploadTask;
+      final String downloadUrl = await taskSnapshot.ref.getDownloadURL();
+      return downloadUrl;
+    } catch (e) {
+      print('Error uploading image: $e');
+      throw e;
+    }
+  }
+
+  Future<void> addJadwalOlahraga(String docId, String nama, DateTime waktu, String lokasi, String imageUrl) async {
+    try {
+      await FirebaseFirestore.instance.collection('jadwal_olahraga').doc(docId).set({
+        'nama': nama,
+        'waktu': waktu,
+        'lokasi': lokasi,
+        'imageUrl': imageUrl,
+      });
+      await fetchJadwalOlahraga();
+    } catch (e) {
+      print('Error adding informasi: $e');
     }
   }
 }
