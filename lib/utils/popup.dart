0c68d9e084504e33a1cccd247285dc00ffc6b965
lyeiva86 <ulul10039@gmail.com>
2024-06-06 22:03:12 +0700

    feat: custom pop up

---
+++ lib/utils/popup.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void showCustomPopup(
+  BuildContext context,
+  String title,
+  String message,
+) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Text(message),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
